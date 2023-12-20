5390a7d638b1235e34658f3daf5e82954ce732d4
fselikat <selikaftm@gmail.com>
2023-12-20 21:33:02 +0300

    19.12.2023 homework

---
+++ homework 19.12.2023/test_SauceDemoHomework.py
@@ -0,0 +1,85 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+
+class Test_SauceDemo:
+    def test_username_required(self):
+        driver = webdriver.Chrome()
+        driver.get("https://www.saucedemo.com")
+        driver.maximize_window() #ekranı büyütür
+        sleep(2)
+        usernameInput = driver.find_element(By.ID,"user-name")
+        usernameInput.send_keys("")
+        sleep(2)
+        passwordInput = driver.find_element(By.ID,"password")
+        passwordInput.send_keys("")
+        sleep(2)
+        loginButton = driver.find_element(By.ID,"login-button")
+        loginButton.click()
+        sleep(2)
+        errorMessage = driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
+        testResult = errorMessage.text == "Epic sadface: Username is required"
+        print(f"TEST 1 SONUCU: {testResult}")
+
+    def test_password_required(self):
+        driver = webdriver.Chrome()
+        driver.get("https://www.saucedemo.com")
+        driver.maximize_window() #ekranı büyütür
+        sleep(2)
+        usernameInput = driver.find_element(By.ID,"user-name")
+        usernameInput.send_keys("standard_user")
+        sleep(2)
+        passwordInput = driver.find_element(By.ID,"password")
+        passwordInput.send_keys("")
+        sleep(2)
+        loginButton = driver.find_element(By.ID,"login-button")
+        loginButton.click()
+        sleep(2)
+        errorMessage = driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
+        testResult = errorMessage.text == "Epic sadface: Password is required"
+        print(f"TEST 2 SONUCU: {testResult}")
+
+    def test_locked_out_user(self):
+        driver = webdriver.Chrome()
+        driver.get("https://www.saucedemo.com")
+        driver.maximize_window() #ekranı büyütür
+        sleep(2)
+        usernameInput = driver.find_element(By.ID,"user-name")
+        usernameInput.send_keys("locked_out_user")
+        sleep(2)
+        passwordInput = driver.find_element(By.ID,"password")
+        passwordInput.send_keys("secret_sauce")
+        sleep(2)
+        loginButton = driver.find_element(By.ID,"login-button")
+        loginButton.click()
+        sleep(2)
+        errorMessage = driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
+        testResult = errorMessage.text == "Epic sadface: Sorry, this user has been locked out."
+        print(f"TEST 3 SONUCU: {testResult}")
+
+    def test_valid_login(self):
+        driver = webdriver.Chrome()
+        driver.get("https://www.saucedemo.com")
+        driver.maximize_window() #ekranı büyütür
+        sleep(2)
+        usernameInput = driver.find_element(By.ID,"user-name")
+        usernameInput.send_keys("standard_user")
+        sleep(2)
+        passwordInput = driver.find_element(By.ID,"password")
+        passwordInput.send_keys("secret_sauce")
+        sleep(2)
+        loginButton = driver.find_element(By.ID,"login-button")
+        loginButton.click()
+        sleep(5)
+        listOfInventories = driver.find_elements(By.CLASS_NAME,"inventory_item")
+        testResult = len(listOfInventories) == 6
+        print(f"TEST 4 SONUCU: {testResult}" )
+
+
+
+testClass = Test_SauceDemo()
+testClass.test_username_required()
+testClass.test_password_required()
+testClass.test_locked_out_user()
+testClass.test_valid_login()
